36f44947f0c48b6f3cc06128a0c11f4976a5e09c
ariful171042 <arif171042@gmail.com>
2023-04-28 22:04:11 +0600

    to do app done

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import TaskList from "./components/TaskList";
 import { createContext, useEffect, useState } from "react";
 
 export const DeleteHandlerContext = createContext();
+export const EditHandlerContext = createContext();
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const [editedText, setEditedText] = useState("");
+  const [toggleEditMode, setToggleEditMode] = useState(true);
 
   useEffect(() => {
     //getting data from the server
@@ -23,11 +29,13 @@ const App = () => {
       const data = await res.json();
 
       setTasks(data);
+      setLoading(false);
     } catch (error) {
-      console.log(error.message);
+      setError(error.message);
     }
   };
 
+  //delete
   const handleDelete = (id) => {
     //delete data
     deleteData(id);
@@ -43,13 +51,71 @@ const App = () => {
     });
     setTasks(tasks.filter((task) => id !== task.id));
   };
+
+  //Edit
+  const handleEdit = (id) => {
+    const [editableTarget] = tasks.filter((task) => id === task.id);
+    editableTarget.isEditable = true;
+    setEditedText(editableTarget.text);
+
+    setTasks([...tasks]);
+    setToggleEditMode(false);
+
+    // re-arrange
+    tasks
+      .filter((task) => task.id !== id)
+      .map((targetedEl) => (targetedEl.isEditable = false));
+  };
+
+  // editing task form handler
+  const handleEditSubmitter = (e, id) => {
+    e.preventDefault();
+
+    setToggleEditMode(!toggleEditMode);
+
+    // persist data
+    const editPersistance = {
+      text: editedText,
+      id: id,
+    };
+
+    // put request
+    puttingRequest(id, editPersistance);
+
+    // real time update
+    const [editableTarget] = tasks.filter((task) => id === task.id);
+    editableTarget.isEditable = false;
+    editableTarget.text = editPersistance.text;
+
+    setTasks([...tasks]);
+  };
+
+  const puttingRequest = async (id, newData) => {
+    fetch(`https://kind-daisy-map.glitch.me/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newData),
+    });
+  };
+
   return (
     <div className="wrapper bg-gradient-to-t from-gray-900 to-teal-900 min-h-screen text-xl text-gray-100 flex flex-col py-10">
       <DeleteHandlerContext.Provider value={handleDelete}>
-        <Header />
-        <AddTask tasks={tasks} setTasks={setTasks} />
-        <TaskList tasks={tasks} />
-        <Footer />
+        <EditHandlerContext.Provider value={handleEdit}>
+          <Header />
+          <AddTask tasks={tasks} setTasks={setTasks} />
+          <TaskList
+            tasks={tasks}
+            error={error}
+            loading={loading}
+            handleEditSubmitter={handleEditSubmitter}
+            editedText={editedText}
+            setEditedText={setEditedText}
+          />
+          <Footer />
+        </EditHandlerContext.Provider>
       </DeleteHandlerContext.Provider>
     </div>
   );
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,22 +1,38 @@
 import { useContext } from "react";
 import { FiEdit } from "react-icons/fi";
 import { MdOutlineDelete } from "react-icons/md";
-import { DeleteHandlerContext } from "../App";
+import { DeleteHandlerContext, EditHandlerContext } from "../App";
 
-const TaskItem = ({ task }) => {
+const TaskItem = ({ task, handleEditSubmitter, editedText, setEditedText }) => {
   const handleDelete = useContext(DeleteHandlerContext);
+  const handleEdit = useContext(EditHandlerContext);
   return (
     <div className="task-item flex justify-between items-center bg-gray-800 p-5 rounded hover:bg-gradient-to-r hover:from-teal-800 hover:to-gray-800 group">
       <div className="task-item-left flex gap-3">
         <span>
           <input type="checkbox" className="accent-teal-400" />
         </span>
-        <p className="group-hover:text-teal-400">{task.text}</p>
+
+        {task.isEditable && (
+          <form onSubmit={(e) => handleEditSubmitter(e, task.id)}>
+            <input
+              className="bg-transparent outline-none border-b-2 border-gray-500 pb-1 focus:border-teal-500"
+              type="text"
+              required
+              value={editedText}
+              onChange={(e) => setEditedText(e.target.value)}
+            />
+          </form>
+        )}
+
+        {!task.isEditable && (
+          <p className="group-hover:text-teal-400">{task.text}</p>
+        )}
       </div>
       <div className="task-item-right flex gap-3">
-        <span>
+        <button onClick={() => handleEdit(task.id)}>
           <FiEdit className="text-gray-500 hover:text-teal-500 cursor-pointer duration-300" />
-        </span>
+        </button>
         <button onClick={() => handleDelete(task.id)}>
           <MdOutlineDelete className="text-gray-500 hover:text-red-500 cursor-pointer duration-300" />
         </button>
